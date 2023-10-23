2ecd26517639dc26b47362d58c3d9ad542fc0b5d
Karol Kurowski <karol.123@vp.pl>
2023-10-23 21:02:01 +0200

    initial commit

---
+++ doggoApp/src/main/java/com/example/doggoApp/doggoApp/DoggoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.doggoApp.doggoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoggoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoggoAppApplication.class, args);
+	}
+
+}
---
+++ doggoApp/src/test/java/com/example/doggoApp/doggoApp/DoggoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doggoApp.doggoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoggoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
